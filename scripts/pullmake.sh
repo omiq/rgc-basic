6511b63e4d41f6809aadbe6d7a0cda60abfe7144
Chris Garrett <chris@chrisg.com>
2026-04-13 15:55:19 +0100

    copy wp assets

--- scripts/pullmake.sh
+++ scripts/pullmake.sh
@@ -51,6 +51,6 @@ make basic-wasm-canvas
 cp -r web/*.* ../8bitworkshop/rgc-basic
 cp -r examples/*.* ../8bitworkshop/presets/rgc-basic/
 cd wordpress/rgc-basic-tutorial-block/
-./copy-web-assets.sh 
+./copy-web-assets.sh /Users/chrisg/github/rgc-basic
 cd ../..
 
